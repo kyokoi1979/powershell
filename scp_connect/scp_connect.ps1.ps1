62e44ef58a8254fef68dd68bcd22e0f929012ac8
Kenta YOKOI <kyokoi@me.com>
2016-02-27 19:29:22 +0900

    add

---
+++ scp_connect/scp_connect.ps1.ps1
@@ -0,0 +1,39 @@
+﻿# WinSCPのパス
+$SCP       = 'C:\Program Files (x86)\WinSCP\WinSCP.exe'
+
+# WinSCPログファイル
+$SCPLOG    = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) winscp.log
+
+# ホストリスト
+$HOST_LIST = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) server.list
+
+# ウェイト
+$WAIT      = 5
+
+$SV = @()
+$HOSTS     = Get-Content $HOST_LIST
+
+function ScpConnect($IP, $USER, $PASS)
+{
+    $ARG = '/default /log=' + $SCPLOG + ' sftp://' + $USER + ':' + $PASS + '@' + $IP
+    Start-Process -FilePath $WINSCP -ArgumentList $ARG
+}
+
+foreach($i in $HOSTS){
+    if (($i -eq "") -Or ($i.substring(0,1) -eq "#")) {
+        Continue
+        }
+    $HOST_IP        = $i | %{ $_.Split("`t")[0]}
+    $HOST_USER      = $i | %{ $_.Split("`t")[1]}
+    $HOST_PASS      = $i | %{ $_.Split("`t")[2]}
+
+    $SERVER = @{ "IP" = $HOST_IP; "USER" = $HOST_USER; "PASS" = $HOST_PASS}
+    New-Object -TypeName PSObject -Property $SERVER -OutVariable result | Out-Null
+
+    $SV += $result
+    }
+
+foreach($i in $SV){
+    ScpConnect $i.IP $i.USER $i.PASS
+    Sleep $WAIT
+}
